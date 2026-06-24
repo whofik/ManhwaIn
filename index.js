5233151d92e7ca2592da89ded6b30a88e33b75b7
F Projects <F1kStore.gamil.1@gmail.com>
2026-06-24 18:41:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const mobinime = new Mobinime()
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'))
