b6aaf74a5c8604c65a4d4c3210d696e354a6107c
FikXzModzDeveloper <F1kStore.gamil.1@gmail.com>
2026-07-19 08:42:53 +0700

    fixed: gatau cape gweh

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
-require('ejs')
-
-const homeRoute = require('./routes/home');
-const searchRoute = require('./routes/search');
-const browseRoute = require('./routes/browse');
-const detailRoute = require('./routes/detail');
+const homeRoute = require('./routes/home')
+const searchRoute = require('./routes/search')
+const browseRoute = require('./routes/browse')
+const detailRoute = require('./routes/detail')
 
 const app = express()
 const port = process.env.PORT || 3001
 
-function resolveDir(rel) {
-    const candidates = [
-        path.join(process.cwd(), rel),
-        path.join(__dirname, rel),
-        path.join(__dirname, '..', rel),
-        path.join(__dirname, '..', '..', rel)
-    ]
-    for (const c of candidates) {
-        if (fs.existsSync(c)) return c
-    }
-    return candidates[0]
-}
-
-app.use(express.static(resolveDir('public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
-app.set('views', resolveDir('views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/', homeRoute);
-app.use('/search', searchRoute);
-app.use('/', browseRoute);
-app.use('/series', detailRoute);
+app.use('/', homeRoute)
+app.use('/search', searchRoute)
+app.use('/', browseRoute)
+app.use('/series', detailRoute)
 app.use((req, res, next) => {
     if (req.path.includes('-chapter-')) {
-        return detailRoute(req, res, next);
+        return detailRoute(req, res, next)
     }
-    next();
-});
+    next()
+})
 
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404 - Tidak Ditemukan' });
-});
+    res.status(404).render('404', { title: '404 - Tidak Ditemukan' })
+})
 
 if (require.main === module) {
     app.listen(port, () => {
