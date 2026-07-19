a7bcc7f3287bfd222045a293b79e66c2baafbc04
FikXzModzDeveloper <F1kStore.gamil.1@gmail.com>
2026-07-19 08:31:09 +0700

    fixed: au ah cape gweh plerr

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+require('ejs')
 
 const homeRoute = require('./routes/home');
 const searchRoute = require('./routes/search');
